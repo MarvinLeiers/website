478c34cb777532c77b8053405435e8071f4befb8
Marvin Leiers <marvinleiers@gmail.com>
2021-08-07 16:55:06 +0200

    Added nav

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
 <!doctype html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>mplugins.org - Free, High-quality Minecraft Plugins</title>
+    <title>Marvin Leiers - Offizielle Webseite</title>
     <link rel="stylesheet" href="style/main.css">
 </head>
 <body>
 <div class="wrapper">
-    <h1>MPlugins</h1>
-
     <nav>
         <ul>
             <li class="link" data-link="home">Home</li>
@@ -25,8 +23,13 @@
     <div id="content">
         <?php include 'pages/home.html'; ?>
     </div>
-
     <script src="scripts/main.js"></script>
 </div>
+<noscript>
+    Please enable JavaScript!
+    <style>
+        .wrapper { display: none; }
+    </style>
+</noscript>
 </body>
 </html>
